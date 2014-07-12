e89b67d829a90e789287b58e987aed51baf19922
Mats391 <m.schoenke@gmail.com>
2014-07-12 12:05:18 +0200

    First commit

---
+++ src/main/java/io/github/mats391/endreset/EndReset.java
@@ -0,0 +1,15 @@
+package io.github.mats391.endreset;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EndReset extends JavaPlugin
+{
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+}
